ec3afa74a5f902ff1d22507e682cb6d3ba49630e
tawnos <jacobrcasey135@gmail.com>
2024-09-05 21:30:40 -0600

    testing

--- line_count.py
+++ line_count.py
@@ -1,9 +1,11 @@
 def line_count():
   # Defines file and open's and reads 'file.txt'
   file = open('file.txt', 'r')
-  # Defines lines and sets lines equal to the length of lines in file
+  # Defines lines and sets lines equal to the length of lines in file.
   lines = len(file.readlines())
-  # This closes the file after it is done defining the length of lines
+  # This closes the file after it is done defining the length of lines.
   file.close()
-  # Returns the value of lines 
+  # Returns the value of lines.
   return lines
+
+#testing git pushing
